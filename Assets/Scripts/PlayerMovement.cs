56ea82485d10658e7551a6ec3b451c5a61641e80
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-27 14:08:56 -0400

    Added Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float runSpeed = 10.0f;
+    public float runSpeed = 100.0f;
+
+    public float jumpSpeed = 200.0f;
 
     private Rigidbody2D m_rigidBody;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+    }
+
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         float horizontal_input = Input.GetAxis("Horizontal");
         m_rigidBody.velocity = new Vector2(horizontal_input * runSpeed * Time.deltaTime, m_rigidBody.velocity.y);
+    }
+
+    void Jump()
+    {
+        m_rigidBody.velocity = new Vector2(m_rigidBody.velocity.x, jumpSpeed);
     }
 }
