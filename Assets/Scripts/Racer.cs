16f1aa337bf78371354c3e28934c00482c8a50e7
Jocelyn Beedie <jellonator00@gmail.com>
2020-11-01 20:51:57 -0500

    Set up scene for 2d sprite overlay

--- Assets/Scripts/Racer.cs
+++ Assets/Scripts/Racer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Racer : MonoBehaviour
 {
+    public float maximumSpeed = 100.0f;
+
+    public float acceleration = 200.0f;
+
+    public float rotationSpeed = 90.0f;
+
+    private Rigidbody2D m_rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontal_input = Input.GetAxis("Horizontal");
+        float vertical_input = Input.GetAxis("Vertical");
+        m_rigidbody.MoveRotation(m_rigidbody.rotation - horizontal_input * rotationSpeed * Time.deltaTime);
     }
 }
