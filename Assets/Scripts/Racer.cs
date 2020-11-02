4726b1ed85347ddda78285acc3cd3bb79bf30094
Jocelyn Beedie <jellonator00@gmail.com>
2020-11-01 21:32:16 -0500

    Programmed player movement

--- Assets/Scripts/Racer.cs
+++ Assets/Scripts/Racer.cs
@@ -23,6 +23,19 @@ public class Racer : MonoBehaviour
     {
         float horizontal_input = Input.GetAxis("Horizontal");
         float vertical_input = Input.GetAxis("Vertical");
+        if (vertical_input < 0.0) {
+            vertical_input = vertical_input * 0.33f;
+        }
         m_rigidbody.MoveRotation(m_rigidbody.rotation - horizontal_input * rotationSpeed * Time.deltaTime);
+        float rad = (m_rigidbody.rotation + 90.0f) * Mathf.Deg2Rad;
+        Vector2 forward = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
+        float currentSpeed = Vector2.Dot(m_rigidbody.velocity, forward);
+        float targetSpeed = vertical_input * maximumSpeed;
+        if (currentSpeed < targetSpeed) {
+            currentSpeed = Mathf.Clamp(currentSpeed + acceleration * Time.deltaTime, currentSpeed, targetSpeed);
+        } else if (currentSpeed > targetSpeed) {
+            currentSpeed = Mathf.Clamp(currentSpeed - acceleration * Time.deltaTime, targetSpeed, currentSpeed);
+        }
+        m_rigidbody.velocity = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)) * currentSpeed;
     }
 }
